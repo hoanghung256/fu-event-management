cc9c5d309fbd3efb2a6401ff1c1f66f0766dd78b
hoanghung256 <hoanghung250604@gmail.com>
2025-06-22 20:10:58 +0700

    init repositories

---
+++ FUEM.Domain/Interfaces/Repositories/IEventGuestRepository.cs
@@ -0,0 +1,13 @@
+﻿using FUEM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUEM.Domain.Interfaces.Repositories
+{
+    public interface IEventGuestRepository
+    {
+    }
+}
