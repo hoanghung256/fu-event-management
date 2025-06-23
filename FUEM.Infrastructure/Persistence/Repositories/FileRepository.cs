b6e2be02201b6dfb9f4466d04b740507d09831e1
hoanghung256 <hoanghung250604@gmail.com>
2025-06-23 16:21:34 +0700

    add repositories implement

---
+++ FUEM.Infrastructure/Persistence/Repositories/FileRepository.cs
@@ -0,0 +1,19 @@
+﻿using FUEM.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUEM.Infrastructure.Persistence.Repositories
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly FUEMDbContext _context;
+
+        public FileRepository(FUEMDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ FUEM.Infrastructure/Persistence/Repositories/LocationRepository.cs
@@ -0,0 +1,19 @@
+﻿using FUEM.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUEM.Infrastructure.Persistence.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly FUEMDbContext _context;
+
+        public LocationRepository(FUEMDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
