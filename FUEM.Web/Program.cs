ee637ba68ecb74953e76ad630fd24c8048155b1d
Nhi Dang <nhidy.it@gmail.com>
2025-07-31 23:40:44 +0700

    update ui bell dropdown

--- FUEM.Web/Program.cs
+++ FUEM.Web/Program.cs
@@ -66,7 +66,7 @@ namespace FUEM.Web
 
             builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDb"));
 
-            builder.Services.AddHostedService<CreateGroupChatService>();
+            //builder.Services.AddHostedService<CreateGroupChatService>();
 
             builder.AddRepositories();
 
